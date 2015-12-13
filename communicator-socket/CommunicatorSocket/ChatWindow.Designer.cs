9d522afa88ae17d0279a2f91861127f725530479
Dariusz Paluch <dariusz.paluch@hotmail.com>
2015-12-13 23:45:03 +0100

    Add Forms: ChatWindow, MainWindow, MessagesWindow

---
+++ communicator-socket/CommunicatorSocket/ChatWindow.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CommunicatorSocket
+{
+    partial class ChatWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessagesRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MessagesRichTextBox
+            // 
+            this.MessagesRichTextBox.Location = new System.Drawing.Point(12, 12);
+            this.MessagesRichTextBox.Name = "MessagesRichTextBox";
+            this.MessagesRichTextBox.Size = new System.Drawing.Size(560, 350);
+            this.MessagesRichTextBox.TabIndex = 0;
+            this.MessagesRichTextBox.Text = "";
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(12, 388);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(425, 54);
+            this.MessageTextBox.TabIndex = 1;
+            this.MessageTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(443, 388);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(129, 55);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Wyślij";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(12, 372);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(92, 13);
+            this.MessageLabel.TabIndex = 3;
+            this.MessageLabel.Text = "Treść wiadomości";
+            this.MessageLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ChatWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.MessagesRichTextBox);
+            this.Name = "ChatWindow";
+            this.Text = "Okno wiadomości - Darek";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox MessagesRichTextBox;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Label MessageLabel;
+
+    }
+}
---
+++ communicator-socket/CommunicatorSocket/ChatWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CommunicatorSocket
+{
+    public partial class ChatWindow : Form
+    {
+        public ChatWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
